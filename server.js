1583ca48427910ea56b4159962661b504c02ba44
linuxnoodle <ipadnoodle@gmail.com>
2021-03-04 03:15:58 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const PORT = 8080;
+
+app.use(express.static("public"));
+
+let userList = [];
+let messages = [];
+
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {});
+  
+    socket.on("addMessage", (name, message) => {
+        messages.push(name + ": " + message);
+        socket.broadcast.emit("newMessage", {username: name, content: message});    
+    });
+
+    socket.on("login", (username) => {
+        userList.push(username);
+        io.to(socket.id).emit("previousMessages", messages);
+    }); 
+});
+
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
---
+++ src/App.svelte
@@ -0,0 +1,70 @@
+{#if !loggedIn}
+    <form action="#" onsubmit="return false">
+        <input bind:value={username} class="form-control" placeholder="Username" id="userName" style="left:0.05vw; bottom:4vh;">
+        <button on:click={login}>login</button>
+    </form>
+{:else}
+    <div id="projects">
+        <div id="projectContainer" bind:this={projectContainer}/>
+        <button on:click={addCard}>add card</button>
+    </div>
+
+    <div id="chat-container">
+        <div class="card">
+            <h1 style="margin-top: 0; text-align: center; color: #eb9b21">{username}</h1>
+            <div id="messageContainer" bind:this={messageContainer}></div>
+        </div>
+    </div>
+
+    <div id="submit">
+        <form action="#" onsubmit="return false">
+            <input style="max-width:19vw; width:100%; bottom:4vh; left: 0px;" id="messageInput" placeholder="Message {username}" type="text" maxlength="55" bind:value={messageText}/>                  
+            <button on:click|preventDefault={sendMessage}>send</button>          
+        </form>                                                     
+    </div>
+{/if}
+<script>
+    let socket = io("ws://linuxnoodle-46718.portmap.io:46718");
+    let username = "";
+    let loggedIn = false;
+
+    let projectContainer;
+    let messageContainer;
+
+    let messageText = "";
+
+    socket.on("newMessage", (message) => {
+        drawMessage(message.username + ": " + message.content);
+        messageContainer.appendChild(element);    
+    });
+
+    socket.on("previousMessages", (messages) => {
+        for (let i = 0; i < messages.length; ++i){
+            drawMessage(messages[i]);
+        }
+    });
+
+    function login(){
+        socket.emit("login", username);
+        loggedIn = true;
+    }
+
+    function sendMessage(){
+        drawMessage(username + ":  " + messageText);
+        socket.emit("addMessage", username, messageText);
+        messageText = "";
+    }
+
+    function drawMessage(content){
+        let element = document.createElement("p");
+        element.textContent = content;
+        messageContainer.appendChild(element);
+    }
+    
+    function addCard(){
+        let element = document.createElement("div"); 
+        element.class = "card";
+        element.appendChild(document.createElement("input"));
+        projectContainer.appendChild(element);
+    }
+</script>
