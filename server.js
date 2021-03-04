b73b118e33ef7771e0d614df2121a70c74ef9460
linuxnoodle <ipadnoodle@gmail.com>
2021-03-04 06:52:46 -0500

    Changed port back to 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const PORT = 80;
+const PORT = 3000;
 
 app.use(express.static("public"));
 
